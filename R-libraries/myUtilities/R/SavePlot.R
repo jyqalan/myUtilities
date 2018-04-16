f180cfc8294e57e445b58e2a2fa2b5a78c2a896d
jyqalan <jyqalan@hotmail.com>
2018-04-16 21:47:52 +0400

    fix SavePlot.R

--- R-libraries/myUtilities/R/SavePlot.R
+++ R-libraries/myUtilities/R/SavePlot.R
@@ -1,5 +1,5 @@
 SavePlot <-
-function(filename="",path="",size=1,width =(21/2.54)*0.8,height = (29.7/2.54)*0.8,pointsize=12,res=300,type="jpg") {
+function(filename="",path="",size=1,width =(21/2.54)*0.8,height = (29.7/2.54)*0.8,pointsize=12,units='in',res=300,type="jpg") {
   # Valid types are JPG (default), PNG, and PDF.
   # the type is automatically added as an extension
   # size represents a fraction of the height of an a4 page
@@ -15,12 +15,12 @@ function(filename="",path="",size=1,width =(21/2.54)*0.8,height = (29.7/2.54)*0.
     stop(paste(type,"is an invalid file type"))
   } else if(Type=="png") {
     filename <- paste(make.filename(filename,path),".png",sep="")
-    png(filename=filename,width=width,height=height*size,units="in",pointsize=pointsize,bg="white",res=res,restoreConsole=TRUE)
+    png(filename=filename,width=width,height=height*size,units=units,pointsize=pointsize,bg="white",res=res,restoreConsole=TRUE)
     replayPlot(this.image)
     dev.off()
   } else if(type=="jpg") {
     filename <- paste(make.filename(filename,path),".jpg",sep="")
-    jpeg(filename=filename,width=width,height=height*size,units="in",quality=100,pointsize=pointsize,bg="white",res=res,restoreConsole=TRUE)
+    jpeg(filename=filename,width=width,height=height*size,units=units,quality=100,pointsize=pointsize,bg="white",res=res,restoreConsole=TRUE)
     replayPlot(this.image)
     dev.off()
   } else if(type=="pdf") {
